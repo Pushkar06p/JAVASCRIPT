df7b3dbd7ae16b6a9fddb49e2871c1c53a427b32
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-11-27 14:03:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treats all js code as newer version
+
+// alert(3+3)  this will give error as we are using node js and not browser
+
+let name="PUSHKAR"
+let age =18
+let isLoggedIn=false
+
+// number=>2^53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone
+// undefined=>
+// symbol=>unique
+
+// object
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
