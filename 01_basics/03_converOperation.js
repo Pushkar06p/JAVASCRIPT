4786d8224df3857e5a9da30b4e0c344f028ad1c7
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-11-27 15:03:44 +0000

    conversion and operations

---
+++ 01_basics/03_converOperation.js
@@ -0,0 +1,66 @@
+// let score = "33abc"
+// console.log(score); //33abc
+
+// console.log(typeof score); //string
+
+// let valueInNumber = Number(score) // changing strings to number
+// console.log(typeof valueInNumber) // number
+// console.log(valueInNumber); // NaN as 33abc can't be changedto number
+
+// score = null
+// console.log(score); //null
+
+// console.log(typeof score); //object
+
+// valueInNumber = String(score) // changing null to string
+// console.log(typeof valueInNumber) // string
+// console.log(valueInNumber); // null
+
+/* converting diifrent datatypes to number
+"33"=>33 
+"33abc"=> NaN
+true => 1
+false => 0
+*/
+
+
+
+
+
+// let isLoggedIn = 12
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/* converting diffrent datatypes to boolean
+1 =>true
+12=>true
+0 => false
+"" => false
+"PUSHKAR" => true
+*/
+
+
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+
+
+// ****************************************Operations***************************************************
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue); // -3
+
+// console.log(2**3); //2^3
+
+// console.log(1+"2"); // 12
+// console.log("1" + 2); // 12
+// console.log("1"+2+2); // 122
+// console.log(1+2+"2"); // 32
+
+console.log(+true);
+console.log(+"");
