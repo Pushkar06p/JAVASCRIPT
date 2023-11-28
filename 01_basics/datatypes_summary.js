933251d9dea8c197a2646d3ae90d624eb6961b7b
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-11-28 18:10:35 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+//  primitive datatype - CALL BY VALUE 
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+//  Reference or non primitive datatypes
+// Array, Objects, Functions
+
+// const heros = ["shaktiman","nagraj", "doga"];
+// let myObj={
+//     name: "PUSHKAR",
+//     age: 20
+// }
+// const myfunction = function(){ 
+//     console.log("Hello World");
+// }
+
+// console.log(typeof myfunction);
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (Primitive) , Heap (Non Primitive)
+
+
+
+
+
+
