20ba161812d5fe6f256158c8131bc49f84a2260e
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-11-28 18:11:04 +0000

    numbers and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,37 @@
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString()); // converting number to string 
+
+// console.log(balance.toString().length); // checking length of converted string
+// console.log(typeof balance.toString()); 
+// console.log(balance.toFixed(3)); // setting precision value upto 3 decimal place
+
+// const otherNumber = 23.56987
+
+// console.log(otherNumber.toFixed(2)); // rounds of after decimal to 2 decimal value
+// console.log(otherNumber.toPrecision(2)); // rounds whole number upto 2 digits
+
+// const hundreds =  100000000
+// console.log(hundreds.toLocaleString()); // provides , in US format
+// console.log(hundreds.toLocaleString("en-IN")); // provides , in Indian format 
+
+
+//++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++++++++
+ 
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+
+// console.log(Math.random()); // gives value between 0 and 1
+// console.log(Math.random() * 10); // gives value between 0 and 10
+// console.log(Math.floor(Math.random() * 10)); // gives value between 0 and 9
+// console.log((Math.floor(Math.random() * 10)) + 1); // gives value between 1 and 10
+
+// const max = 25;
+// const min = 12;
+
+// console.log((Math.floor(Math.random() *(max-min))) + min); // gives value between min and max
+
+
+
