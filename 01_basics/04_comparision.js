a75972970222efd525e8baf68dc3a3aea6c072fd
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-11-28 15:07:20 +0000

    comparisons

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0); // false         // avoid these type of conversion 
+// console.log(null == 0); // false        // avoid these type of conversion 
+// console.log(null >= 0); // true         // avoid these type of conversion 
+
+// console.log(undefined == 0); // false     // avoid these type of conversion 
+// console.log(undefined >= 0); // false    // avoid these type of conversion 
+// console.log(undefined > 0); // false     // avoid these type of conversion 
+
+// === it is strict check it also checks datatypes and not converts one data type to other
+console.log("2" == 2); // true
+console.log("2" === 2); // false
