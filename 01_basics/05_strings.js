2ff12f4b61100301d544abe6ebcca32f76400fb0
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-11-28 18:10:14 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "PUSHHKAR"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value"); // old syntax
+
+// console.log(`Hello my name is ${name} and my repoacount is ${repoCount}`); // new syntax
+
+// const gameName = new String('PUSHKARP')
+
+// console.log(gameName.toLowerCase());
+// console.log(gameName.slice(-6,6)); // same as substring but in slice we can put negative integer also 
+
+
+// console.log("    pushkar   ");
+// console.log("    pushkar   ".trim()); // removes starting and ending spaces
+// console.log("    pushkar   ".trimStart()); // removes starting  spaces
+// console.log("    pushkar   ".trimEnd()); // removes ending spaces
+
+
+console.log("https://pushkar.com/pushkar%20pandey");
+
+console.log("https://pushkar.com/pushkar%20pandey".includes("pushkar"));
+console.log("https://pushkar.com/pushkar%20pandey".includes("hitesh")); //searches whether substring is present in a given string or not
+
+// console.log("https://pushkar.com/pushkar%20pandey".replace("%20","-")); // replace given substring with another substring
+
+
+
+
+
+
+
+
+
