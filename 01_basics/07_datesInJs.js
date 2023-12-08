c92fb28b436e4fe62838d2f9eb991c27d26853a3
Pushkar06p <152164252+Pushkar06p@users.noreply.github.com>
2023-12-08 18:32:57 +0000

    dates in javascript

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,30 @@
+//  Dates
+
+let myDate = new Date();
+// console.log(myDate.toString()); // Mon Dec 04 2023 18:30:01 GMT+0000
+// console.log(myDate.toDateString());  // Mon Dec 04 2023
+// console.log(myDate.toLocaleString()); // 12/4/2023, 6:33:34 PM
+// console.log(myDate.toLocaleDateString()); // 12/4/2023
+// console.log(myDate.toISOString()); // 2023-12-04T18:33:34.916Z
+// console.log(myDate.toJSON()); // 2023-12-04T18:33:34.916Z
+// console.log(myDate.toLocaleTimeString()); // 6:33:34 PM
+
+// console.log(typeof myDate); // object
+
+// let myCreatedDate = new Date(2023, 4, 23);
+// console.log(myCreatedDate.toDateString()); // Tue May 23 2023
+
+// let myCreatedDate = new Date(2023, 4, 23, 13, 3, 35);
+// console.log(myCreatedDate.toLocaleString()); // 5/23/2023, 1:03:35 PM
+
+// let myCreatedDate = new Date("01-14-2023")
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000)); // converting into seceonds
+
+// let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth() + 1);
+
